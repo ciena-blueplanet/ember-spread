7526c6a630d3bc4489f05829ff012ddde964a6e8
Matthew Dahl <mdahl@ciena.com>
2017-01-24 22:50:57 -0800

    Minor cleanup

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -17,6 +17,12 @@ module.exports = function (environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    'ember-prop-types': {
+      spreadProperty: 'options',
+      throwErrors: true,
+      validateOnUpdate: true
     }
   }
 
