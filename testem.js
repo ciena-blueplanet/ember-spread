e7d408a12170604b287d0b6aac98206e2b7339a4
Michael Carroll <juwara0@users.noreply.github.com>
2017-02-20 13:10:23 -0600

    udpate to run firefox in ci

--- testem.js
+++ testem.js
@@ -1,11 +1,15 @@
+var Reporter = require('ember-test-utils/reporter')
+
 module.exports = {
-  'framework': 'mocha',
-  'test_page': 'tests/index.html?hidepassed',
-  'disable_watching': true,
-  'launch_in_ci': [
+  disable_watching: true,
+  framework: 'mocha',
+  launch_in_ci: [
+    'Chrome',
+    'Firefox'
+  ],
+  launch_in_dev: [
     'Chrome'
   ],
-  'launch_in_dev': [
-    'Chrome'
-  ]
+  reporter: new Reporter(),
+  test_page: 'tests/index.html?hidepassed'
 }
