33c6cd040fa9c0c832fcf76fa145ace43892f21d
Michael Carroll <juwara0@users.noreply.github.com>
2017-03-07 10:23:24 -0600

    update travis scripts for bumping and publish

---
+++ .travis/maybe-bump-version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$TRAVIS_NODE_VERSION" != "6.9.1" ]
+then
+  echo "Skipping version bump for TRAVIS_NODE_VERSION ${TRAVIS_NODE_VERSION}"
+  exit 0
+fi
+
+if [ "$EMBER_TRY_SCENARIO" != "default" ]
+then
+  echo "Skipping version bump for EMBER_TRY_SCENARIO ${EMBER_TRY_SCENARIO}"
+  exit 0
+fi
+
+$(npm root -g)/pr-bumper/.travis/maybe-bump-version.sh
