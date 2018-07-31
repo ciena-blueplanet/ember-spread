24493cbe8cea7022c64e18a5fdada3aa10c3c21b
Quincy Le <lexuqing8@gmail.com>
2018-07-31 16:39:20 -0400

    fix conflict

--- tests/integration/components/spread-test.js
+++ tests/integration/components/spread-test.js
@@ -294,7 +294,6 @@ describe(test.label, function () {
 
     describe('and source property is replaced by an empty object', function () {
       beforeEach(function () {
-<<<<<<< HEAD
         this.set('options', Ember.Object.create({}))
       })
 
@@ -307,11 +306,6 @@ describe(test.label, function () {
           this.set('options', Ember.Object.create({
             property: 'foo'
           }))
-=======
-        this.setProperties({
-          options: Ember.Object.create({}),
-          condition: true
->>>>>>> parent of c97687e... remove debugger
         })
 
         it('should update the local property', function () {
