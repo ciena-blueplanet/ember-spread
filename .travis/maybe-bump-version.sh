8b5901201952e268b0db20b410267daff93e9385
Kevin Ho <kevinho@ciena.com>
2018-10-11 16:16:59 -0400

    Travis and dependencies

--- .travis/maybe-bump-version.sh
+++ .travis/maybe-bump-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ! "${PUBLISH_NODE_VERSION:-8.1.2}" =~ ^$TRAVIS_NODE_VERSION ]]
+if [[ ! "${PUBLISH_NODE_VERSION:-10.7.0}" =~ ^$TRAVIS_NODE_VERSION ]]
 then
   echo "Skipping pr-bumper bump step for TRAVIS_NODE_VERSION [${TRAVIS_NODE_VERSION}]"
   exit 0
